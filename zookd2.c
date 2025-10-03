2142f09de364ad78fe4b64e8bd2d899117d218ec
AleSierra26 <alemsierra26@uc.cl>
2025-10-03 15:56:19 -0300

    a

--- zookd2.c
+++ zookd2.c
@@ -1,5 +1,5 @@
 /* dispatch daemon */
-
+/* listens for connections from clients */
 #include "http2.h"
 #include <err.h>
 #include <regex.h>
@@ -21,123 +21,124 @@ static int svcports[MAX_SERVICES];
 
 static void process_client(int);
 static int start_daemon(int fd, int sockfd);
-  
+
 int main(int argc, char **argv)
 {
-    if (argc != 3)
-        errx(1, "Wrong arguments");
-    start_daemon(atoi(argv[1]), atoi(argv[2]));
+  if (argc != 3)
+    errx(1, "Wrong arguments");
+  start_daemon(atoi(argv[1]), atoi(argv[2]));
 }
 
-static int start_daemon(int fd, int sockfd) {
-    int i;
+static int start_daemon(int fd, int sockfd)
+{
+  int i;
 
-    signal(SIGPIPE, SIG_IGN);
-    signal(SIGCHLD, SIG_IGN);
+  signal(SIGPIPE, SIG_IGN);
+  signal(SIGCHLD, SIG_IGN);
 
-    /* receive the number of services and the server socket from zookld */
-    if (read(fd, &nsvcs, sizeof(nsvcs)) != sizeof(nsvcs))
-        err(1, "read nsvcs");
-    warnx("Start with %d service(s)", nsvcs);
+  /* receive the number of services and the server socket from zookld */
+  if (read(fd, &nsvcs, sizeof(nsvcs)) != sizeof(nsvcs))
+    err(1, "read nsvcs");
+  warnx("Start with %d service(s)", nsvcs);
 
-    /* receive url patterns of all services */
-    for (i = 0; i != nsvcs; ++i)
+  /* receive url patterns of all services */
+  for (i = 0; i != nsvcs; ++i)
+  {
+    char url[1024], regexp[1028];
+    memset(url, '\0', sizeof(url));
+    int len = 0;
+    int n;
+    if ((n = read(fd, &len, sizeof(len))) != sizeof(len))
+      err(1, "recv urllen %d %d", i, n);
+    if (read(fd, url, len) != len)
+      err(1, "recv url %d", i);
+    /* parens are necessary here so that regexes like a|b get
+       parsed properly and not as (^a)|(b$) */
+    snprintf(regexp, sizeof(regexp), "^(%s)$", url);
+    if (regcomp(&svcurls[i], regexp, REG_EXTENDED | REG_NOSUB))
+      errx(1, "Bad url for service %d: %s", i, url);
+    warnx("Dispatch %s for service %d", regexp, i);
+  }
+
+  /* receive hosts of all services */
+  for (i = 0; i != nsvcs; ++i)
+  {
+    int link = 0;
+    if (read(fd, &link, sizeof(link)) != sizeof(link))
+      err(1, "recv link %d failed", i);
+    snprintf(svchosts[i], sizeof(svchosts[i]), "10.1.%d.4", link);
+    warnx("Host %s (link %d) service %d", svchosts[i], link, i);
+  }
+
+  /* receive ports of all services */
+  for (i = 0; i != nsvcs; ++i)
+  {
+    if (read(fd, &(svcports[i]), sizeof(svcports[i])) != sizeof(svcports[i]))
+      err(1, "recv port %d failed", i);
+    warnx("Port %d for service %d", svcports[i], i);
+  }
+
+  close(fd);
+
+  // Make sure there are no buffered writes before forking
+  fflush(stdout);
+  fflush(stderr);
+
+  for (;;)
+  {
+    int cltfd = accept(sockfd, NULL, NULL);
+    if (cltfd < 0)
+      err(1, "accept");
+    switch (fork())
     {
-        char url[1024], regexp[1028];
-	memset(url, '\0', sizeof(url));
-	int len = 0;
-	int n;
-	if ((n = read(fd, &len, sizeof(len))) != sizeof(len))
-	  err(1, "recv urllen %d %d", i, n);
-        if (read(fd, url, len) != len)
-	  err(1, "recv url %d", i);
-       /* parens are necessary here so that regexes like a|b get
-          parsed properly and not as (^a)|(b$) */
-        snprintf(regexp, sizeof(regexp), "^(%s)$", url);
-        if (regcomp(&svcurls[i], regexp, REG_EXTENDED | REG_NOSUB))
-            errx(1, "Bad url for service %d: %s", i, url);
-        warnx("Dispatch %s for service %d", regexp, i);
+    case -1: /* error */
+      err(1, "fork");
+    case 0: /* child */
+      process_client(cltfd);
+      return 0;
+    default: /* parent */
+      close(cltfd);
+      break;
     }
-
-    /* receive hosts of all services */
-    for (i = 0; i != nsvcs; ++i)
-    {
-      int link = 0;
-      if (read(fd, &link, sizeof(link)) != sizeof(link))
-	err(1, "recv link %d failed", i);
-      snprintf(svchosts[i], sizeof(svchosts[i]), "10.1.%d.4", link);
-      warnx("Host %s (link %d) service %d", svchosts[i], link, i);
-    }
-
-    /* receive ports of all services */
-    for (i = 0; i != nsvcs; ++i)
-    {
-      if (read(fd, &(svcports[i]), sizeof(svcports[i])) != sizeof(svcports[i]))
-	err(1, "recv port %d failed", i);
-      warnx("Port %d for service %d", svcports[i], i);
-    }
-
-    close(fd);
-
-    // Make sure there are no buffered writes before forking
-    fflush(stdout);
-    fflush(stderr);
-
-    for (;;)
-    {
-        int cltfd = accept(sockfd, NULL, NULL);
-        if (cltfd < 0)
-            err(1, "accept");
-        switch (fork())
-        {
-        case -1: /* error */
-            err(1, "fork");
-        case 0:  /* child */
-            process_client(cltfd);
-            return 0;
-        default: /* parent */
-            close(cltfd);
-            break;
-        }
-    }
+  }
 }
 
 int read_line(int fd, char *buf, size_t size, int eoh)
 {
-    size_t i = 0;
-    for (;;)
+  size_t i = 0;
+  for (;;)
+  {
+    int cc = read(fd, &buf[i], 1);
+    if (cc < 0)
+      break;
+
+    if (cc == 0)
+      return 0;
+
+    if (eoh && i == 1 && buf[0] == '\r' && buf[1] == '\n')
     {
-        int cc = read(fd, &buf[i], 1);
-        if (cc < 0) 
-            break;
-	
-	if (cc == 0)
-	  return 0;
-
-	if (eoh && i == 1 && buf[0] == '\r' && buf[1] == '\n')
-	{
-	  return 0;
-	}
-
-	if (buf[i] == '\n')
-        {
-            return i+1;
-        }
-
-        if (i >= size - 1)
-        {
-            buf[i] = '\0';
-            return i+1;
-        }
-        i++;
+      return 0;
     }
-    return -1;
+
+    if (buf[i] == '\n')
+    {
+      return i + 1;
+    }
+
+    if (i >= size - 1)
+    {
+      buf[i] = '\0';
+      return i + 1;
+    }
+    i++;
+  }
+  return -1;
 }
 
 static int proxy(int i, int fd, char *env, size_t env_len)
 {
   static char buf[8192];  /* static variables are not on the stack */
-  static char buf1[8192];  /* static variables are not on the stack */
+  static char buf1[8192]; /* static variables are not on the stack */
   int sockfd, portno;
   struct sockaddr_in servaddr;
   char *server;
@@ -154,62 +155,72 @@ static int proxy(int i, int fd, char *env, size_t env_len)
   server = &(svchosts[i][0]);
   portno = svcports[i];
 
-  bzero((char *) &servaddr, sizeof(servaddr));
+  bzero((char *)&servaddr, sizeof(servaddr));
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = inet_addr(server);
   servaddr.sin_port = htons(portno);
-  if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+  {
     warn("connect failure to svc %d", i);
     return -1;
   }
-  
-  if (write(sockfd, &env_len, sizeof(env_len)) < 0) {
+
+  if (write(sockfd, &env_len, sizeof(env_len)) < 0)
+  {
     warn("write to svc %d failed", i);
   }
 
-  if (write(sockfd, env, env_len) < 0) {
+  if (write(sockfd, env, env_len) < 0)
+  {
     warn("write to svc %d failed", i);
   }
 
   // proxy header
-  while(1) {
+  while (1)
+  {
     memset(buf, '\0', sizeof(buf));
-    if ((n = read_line(fd, buf, sizeof(buf), 1)) < 0) {
+    if ((n = read_line(fd, buf, sizeof(buf), 1)) < 0)
+    {
       warn("read_line from client failed");
       return -1;
     }
 
-    if (n == 0) {
+    if (n == 0)
+    {
       write(sockfd, buf, 2);
       break;
     }
 
     memcpy(buf1, buf, sizeof(buf));
 
-    if ((r = http_parse_line(buf1, envvar, value)) != NULL) {
+    if ((r = http_parse_line(buf1, envvar, value)) != NULL)
+    {
       warn("parse_line: invalid");
       return -1;
     }
 
-    if (strcmp(envvar, "CONTENT_LENGTH") == 0) {
+    if (strcmp(envvar, "CONTENT_LENGTH") == 0)
+    {
       len = atoi(value);
     }
-        
-    
-    if (write(sockfd, buf, n) != n) {
+
+    if (write(sockfd, buf, n) != n)
+    {
       warn("write to svc failed");
       return -1;
-    
     }
   }
 
   // proxy body
-  while (len > 0) {
-    if ((n = read(fd, buf, sizeof(buf))) < 0) {
-	warn("read from svc failed");
-	return -1;
+  while (len > 0)
+  {
+    if ((n = read(fd, buf, sizeof(buf))) < 0)
+    {
+      warn("read from svc failed");
+      return -1;
     }
-    if (write(sockfd, buf, n) != n) {
+    if (write(sockfd, buf, n) != n)
+    {
       warn("write body to client failed");
       return -1;
     }
@@ -217,16 +228,20 @@ static int proxy(int i, int fd, char *env, size_t env_len)
   }
 
   // proxy response
-  while (1) {
+  while (1)
+  {
     memset(buf, '\0', sizeof(buf));
-    if ((n = read(sockfd, buf, sizeof(buf))) < 0) {
-	warn("read from svc failed");
-	return -1;
+    if ((n = read(sockfd, buf, sizeof(buf))) < 0)
+    {
+      warn("read from svc failed");
+      return -1;
     }
-    if (n == 0) {
+    if (n == 0)
+    {
       break;
     }
-    if (write(fd, buf, n) != n) {
+    if (write(fd, buf, n) != n)
+    {
       warn("write body to client failed");
       return -1;
     }
@@ -235,39 +250,37 @@ static int proxy(int i, int fd, char *env, size_t env_len)
   return 0;
 }
 
-
 static void process_client(int fd)
 {
-    static char env[8192];  /* static variables are not on the stack */
-    static size_t env_len = 8192;
-    char reqpath[4096];
-    const char *errmsg;
+  static char env[8192]; /* static variables are not on the stack */
+  static size_t env_len = 8192;
+  char reqpath[4096];
+  const char *errmsg;
 
-    /* get the request line */
-    if ((errmsg = http_request_line(fd, reqpath, env, &env_len)))
-        return http_err(fd, 500, "http_request_line: %s", errmsg);
+  /* get the request line */
+  if ((errmsg = http_request_line(fd, reqpath, env, &env_len)))
+    return http_err(fd, 500, "http_request_line: %s", errmsg);
 
-    int i;
-    for (i = 0; i < nsvcs; i++)
+  int i;
+  for (i = 0; i < nsvcs; i++)
+  {
+    if (!regexec(&svcurls[i], reqpath, 0, 0, 0))
     {
-        if (!regexec(&svcurls[i], reqpath, 0, 0, 0))
-        {
-            warnx("Forwarding to %s:%d for %s", svchosts[i], svcports[i], reqpath);
-            break;
-        }
+      warnx("Forwarding to %s:%d for %s", svchosts[i], svcports[i], reqpath);
+      break;
     }
+  }
 
-    if (i == nsvcs)
-        return http_err(fd, 500, "Error dispatching request: %s", reqpath);
+  if (i == nsvcs)
+    return http_err(fd, 500, "Error dispatching request: %s", reqpath);
 
-    if (proxy(i, fd, env, env_len) < 0) 
-      return http_err(fd, 500, "Error proxying request: %s", reqpath);
+  if (proxy(i, fd, env, env_len) < 0)
+    return http_err(fd, 500, "Error proxying request: %s", reqpath);
 
-    close(fd);
+  close(fd);
 }
-
 
 void accidentally(void)
 {
-       __asm__("mov 16(%%rbp), %%rdi": : :"rdi");
+  __asm__("mov 16(%%rbp), %%rdi" : : : "rdi");
 }
