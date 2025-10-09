8f25b89ecd3e140a46df12f857813a3f85a7cd75
AleSierra26 <alemsierra26@uc.cl>
2025-10-09 12:46:05 -0300

    test

--- zoobar/auth.py
+++ zoobar/auth.py
@@ -1,43 +1,39 @@
 from zoodb import *
 from debug import *
+import hashlib, random
 
-import hashlib
-import random
-
-def newtoken(db, person):
-    hashinput = "%s%.10f" % (person.password, random.random())
-    person.token = hashlib.md5(hashinput.encode('utf-8')).hexdigest()
+def newtoken(db, cred):
+    hashinput = "%s%.10f" % (cred.password, random.random())
+    cred.token = hashlib.md5(hashinput.encode('utf-8')).hexdigest()
     db.commit()
-    return person.token
+    return cred.token
 
 def login(username, password):
-    db = person_setup()
-    person = db.query(Person).get(username)
-    if not person:
+    db = cred_setup()
+    cred = db.query(Cred).get(username)
+    if not cred:
         return None
-    if person.password == password:
-        return newtoken(db, person)
+    if cred.password == password:
+        return newtoken(db, cred)
     else:
         return None
 
 def register(username, password):
-    db = person_setup()
-    person = db.query(Person).get(username)
-    if person:
+    db = cred_setup()
+    cred = db.query(Cred).get(username)
+    if cred:
         return None
-    newperson = Person()
-    newperson.username = username
-    newperson.password = password
-    db.add(newperson)
+    newcred = Cred()
+    newcred.username = username
+    newcred.password = password
+    db.add(newcred)
     db.commit()
-    return newtoken(db, newperson)
+    return newtoken(db, newcred)
 
 def check_token(username, token):
-    db = person_setup()
-    person = db.query(Person).get(username)
-    if person and person.token == token:
+    db = cred_setup()
+    cred = db.query(Cred).get(username)
+    if cred and cred.token == token:
         return True
     else:
         return False
-
-    
