e09fa2a1c9d31757c9942eb352c49ceeec0b5f47
AleSierra26 <alemsierra26@uc.cl>
2025-10-09 12:26:08 -0300

    test

--- zoobar/auth_client.py
+++ zoobar/auth_client.py
@@ -1,15 +1,26 @@
 from debug import *
 from zoodb import *
 import rpclib
+import sys
 
 sys.path.append(os.getcwd())
 import readconf
 
+def _get_auth_host():
+    conf = readconf.read_conf()
+    return conf.lookup_host('auth')
+
 def login(username, password):
-    ## Fill in code here.
+    host = _get_auth_host()
+    with rpclib.client_connect(host) as c:
+        return c.call('login', username=username, password=password)
 
 def register(username, password):
-    ## Fill in code here.
+    host = _get_auth_host()
+    with rpclib.client_connect(host) as c:
+        return c.call('register', username=username, password=password)
 
 def check_token(username, token):
-    ## Fill in code here.
+    host = _get_auth_host()
+    with rpclib.client_connect(host) as c:
+        return c.call('check_token', username=username, token=token)
--- zoobar/zoodb.py
+++ zoobar/zoodb.py
@@ -6,14 +6,19 @@ from debug import *
 
 PersonBase = declarative_base()
 TransferBase = declarative_base()
+CredBase = declarative_base()
 
 class Person(PersonBase):
     __tablename__ = "person"
     username = Column(String(128), primary_key=True)
-    password = Column(String(128))
-    token = Column(String(128))
     zoobars = Column(Integer, nullable=False, default=10)
     profile = Column(String(5000), nullable=False, default="")
+
+class Cred(CredBase):
+    __tablename__ = "cred"
+    username = Column(String(128), primary_key=True)
+    password = Column(String(128))
+    token = Column(String(128))
 
 class Transfer(TransferBase):
     __tablename__ = "transfer"
@@ -38,6 +43,9 @@ def dbsetup(name, base):
 
 def person_setup():
     return dbsetup("person", PersonBase)
+
+def cred_setup():
+    return dbsetup("cred", CredBase)
 
 def transfer_setup():
     return dbsetup("transfer", TransferBase)
