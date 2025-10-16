189c313eb9d1122e2495152e548dc484731e0f17
AleSierra26 <alemsierra26@uc.cl>
2025-10-16 08:31:47 -0300

    volver

--- zoobar/auth-server.py
+++ zoobar/auth-server.py
@@ -6,14 +6,8 @@ import auth
 from debug import *
 
 class AuthRpcServer(rpclib.RpcServer):
-    def rpc_login(self,username,password):
-        return auth.login(username,password)
-
-    def rpc_register(self,username,password):
-        return auth.register(username,password)
-        
-    def rpc_check_token(self,username,token):
-        return auth.check_token(username,token)
+    ## Fill in RPC methods here.
+    pass
 
 if len(sys.argv) != 2:
     print(sys.argv[0], "too few args")
--- zoobar/auth_client.py
+++ zoobar/auth_client.py
@@ -1,20 +1,15 @@
 from debug import *
+from zoodb import *
 import rpclib
-import sys, os
+
 sys.path.append(os.getcwd())
 import readconf
 
 def login(username, password):
-    host = readconf.read_conf().lookup_host('auth')
-    with rpclib.client_connect(host) as c:
-        return c.call('login', username=username, password=password)
+    ## Fill in code here.
 
 def register(username, password):
-    host = readconf.read_conf().lookup_host('auth')
-    with rpclib.client_connect(host) as c:
-        return c.call('register', username=username, password=password)
+    ## Fill in code here.
 
 def check_token(username, token):
-    host = readconf.read_conf().lookup_host('auth')
-    with rpclib.client_connect(host) as c:
-        return c.call('check_token', username=username, token=token)
+    ## Fill in code here.
