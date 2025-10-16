da07909e2c3b4a0a7869fac02629ce4a107f2cf0
AleSierra26 <alemsierra26@uc.cl>
2025-10-16 08:27:47 -0300

    test con profe

--- zoobar/auth_client.py
+++ zoobar/auth_client.py
@@ -1,15 +1,20 @@
 from debug import *
-from zoodb import *
 import rpclib
-
+import sys, os
 sys.path.append(os.getcwd())
 import readconf
 
 def login(username, password):
-    ## Fill in code here.
+    host = readconf.read_conf().lookup_host('auth')
+    with rpclib.client_connect(host) as c:
+        return c.call('login', username=username, password=password)
 
 def register(username, password):
-    ## Fill in code here.
+    host = readconf.read_conf().lookup_host('auth')
+    with rpclib.client_connect(host) as c:
+        return c.call('register', username=username, password=password)
 
 def check_token(username, token):
-    ## Fill in code here.
+    host = readconf.read_conf().lookup_host('auth')
+    with rpclib.client_connect(host) as c:
+        return c.call('check_token', username=username, token=token)
