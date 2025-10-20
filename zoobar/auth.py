111e827948262fe880e01b8411027dde314e3089
AleSierra26 <alemsierra26@uc.cl>
2025-10-20 19:37:46 -0300

    a ver

--- zoobar/auth.py
+++ zoobar/auth.py
@@ -18,36 +18,40 @@ def login(username, password):
     person = db_person.query(Person).get(username)
     if not person:
         return None
+
     db_cred = cred_setup()
     cred = db_cred.query(Cred).get(username)
-    password = pbkdf2.PBKDF2(password,cred.salt).hexread(32)
-    if cred.password == password:
-        return newtoken(db_cred, cred)
-    else:
+    if not cred:
         return None
+
+    salt_bytes = bytes.fromhex(cred.salt)
+    hashed = pbkdf2.PBKDF2(password, salt_bytes).hexread(32)
+
+    if cred.password == hashed:
+        return newtoken(db_cred, cred)
+    return None
+
 
 def register(username, password):
     db_person = person_setup()
     person = db_person.query(Person).get(username)
     if person:
         return None
-    newperson = Person()
-    newperson.username = username
-    db_person.add(newperson)
-    db_person.commit()
-    
-    salt = os.urandom(32).hex()
 
-    password = pbkdf2.PBKDF2(password,salt).hexread(32) 
- 
+    salt = os.urandom(32)
+    hashed = pbkdf2.PBKDF2(password, salt).hexread(32)
+
     db_cred = cred_setup()
     newcred = Cred()
     newcred.username = username
-    newcred.password = password
-    newcred.salt = salt
+    newcred.password = hashed
+    newcred.salt = salt.hex()
     db_cred.add(newcred)
     db_cred.commit()
+
     return newtoken(db_cred, newcred)
+
+
 
 def check_token(username, token):
     db = cred_setup()
--- zoobar/auth_client.py
+++ zoobar/auth_client.py
@@ -12,7 +12,18 @@ def login(username, password):
 def register(username, password):
     host = readconf.read_conf().lookup_host('auth')
     with rpclib.client_connect(host) as c:
-        return c.call('register', username=username, password=password)
+        token = c.call('register', username=username, password=password)
+
+    if token:
+        from zoodb import person_setup, Person
+        db_person = person_setup()
+        if not db_person.query(Person).get(username):
+            newperson = Person()
+            newperson.username = username
+            db_person.add(newperson)
+            db_person.commit()
+    return token
+
 
 def check_token(username, token):
     host = readconf.read_conf().lookup_host('auth')
