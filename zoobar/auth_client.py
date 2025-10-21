cce04179b4f426dcd71013d4ce4eeea07930078c
AleSierra26 <alemsierra26@uc.cl>
2025-10-21 20:52:12 -0300

    lol

--- zoobar/auth_client.py
+++ zoobar/auth_client.py
@@ -1,20 +1,46 @@
+# zoobar/auth_client.py
 from debug import *
 import rpclib
 import sys, os
 sys.path.append(os.getcwd())
 import readconf
 
+def _connect_to_auth():
+    # lookup_host normalmente devuelve una tupla (ip, port) o similar
+    host = readconf.read_conf().lookup_host('auth')
+    # defensive: si lookup_host devuelve un objeto con campos, adaptarlo
+    if isinstance(host, tuple) or isinstance(host, list):
+        return rpclib.client_connect((host[0], int(host[1])))
+    # si lookup_host ya devolviera un string "ip:port", intentar parsearlo
+    if isinstance(host, str) and ':' in host:
+        ip, port = host.split(':', 1)
+        return rpclib.client_connect((ip, int(port)))
+    # fallback: tratar de conectar a localhost:8081
+    return rpclib.client_connect(('127.0.0.1', 8081))
 
-def login(username, password) :
-    host = readconf.read_conf().lookup_host('auth')
+def login(username, password):
     kwargs = {'username': username, 'password': password}
-    return rpclib.client_connect(('10.1.3.4', 8081)).call('login', **kwargs)
-def register(username, password) :
-    host = readconf.read_conf().lookup_host('auth')
+    try:
+        conn = _connect_to_auth()
+        return conn.call('login', **kwargs)
+    except Exception as e:
+        debug("auth_client.login: RPC error: %r" % (e,))
+        return None
+
+def register(username, password):
     kwargs = {'username': username, 'password': password}
-    return rpclib.client_connect(('10.1.3.4', 8081)).call('register', **kwargs)
-def check_token(username, token) :
-    host = readconf.read_conf().lookup_host('auth')
+    try:
+        conn = _connect_to_auth()
+        return conn.call('register', **kwargs)
+    except Exception as e:
+        debug("auth_client.register: RPC error: %r" % (e,))
+        return None
+
+def check_token(username, token):
     kwargs = {'username': username, 'token': token}
-    return rpclib.client_connect(('10.1.3.4', 8081)).call('check_token', **kwargs)
-    
+    try:
+        conn = _connect_to_auth()
+        return conn.call('check_token', **kwargs)
+    except Exception as e:
+        debug("auth_client.check_token: RPC error: %r" % (e,))
+        return False
