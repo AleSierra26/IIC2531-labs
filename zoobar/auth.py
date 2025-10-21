54832b1e54ca80a2975def5d4859fa6f3a6ad232
AleSierra26 <alemsierra26@uc.cl>
2025-10-21 20:41:20 -0300

    gepeto

--- zoobar/auth.py
+++ zoobar/auth.py
@@ -3,41 +3,60 @@ from debug import *
 
 import hashlib
 import random
+import os
+import pbkdf2
 
-def newtoken(db, person):
-    hashinput = "%s%.10f" % (person.password, random.random())
-    person.token = hashlib.md5(hashinput.encode('utf-8')).hexdigest()
+def newtoken(db, cred):
+    hashinput = "%s%.10f" % (cred.password, random.random())
+    cred.token = hashlib.md5(hashinput.encode()).hexdigest()
+
     db.commit()
-    return person.token
+    return cred.token
 
 def login(username, password):
-    db = person_setup()
-    person = db.query(Person).get(username)
+    db_person = person_setup()
+    person = db_person.query(Person).get(username)
     if not person:
         return None
-    if person.password == password:
-        return newtoken(db, person)
-    else:
+
+    db_cred = cred_setup()
+    cred = db_cred.query(Cred).get(username)
+    if not cred:
         return None
+
+    salt_bytes = bytes.fromhex(cred.salt)
+    hashed = pbkdf2.PBKDF2(password, salt_bytes).hexread(32)
+
+    if cred.password == hashed:
+        return newtoken(db_cred, cred)
+    return None
+
 
 def register(username, password):
-    db = person_setup()
-    person = db.query(Person).get(username)
+    db_person = person_setup()
+    person = db_person.query(Person).get(username)
     if person:
         return None
-    newperson = Person()
-    newperson.username = username
-    newperson.password = password
-    db.add(newperson)
-    db.commit()
-    return newtoken(db, newperson)
+
+    salt = os.urandom(32)
+    hashed = pbkdf2.PBKDF2(password, salt).hexread(32)
+
+    db_cred = cred_setup()
+    newcred = Cred()
+    newcred.username = username
+    newcred.password = hashed
+    newcred.salt = salt.hex()
+    db_cred.add(newcred)
+    db_cred.commit()
+
+    return newtoken(db_cred, newcred)
+
+
 
 def check_token(username, token):
-    db = person_setup()
-    person = db.query(Person).get(username)
-    if person and person.token == token:
+    db = cred_setup()
+    cred = db.query(Cred).get(username)
+    if cred and cred.token == token:
         return True
     else:
         return False
-
-    
--- zoobar/login.py
+++ zoobar/login.py
@@ -3,7 +3,7 @@ from functools import wraps
 from debug import *
 from zoodb import *
 
-import auth
+import auth_client as auth
 import bank
 import random
 
@@ -28,6 +28,13 @@ class User(object):
     def addRegistration(self, username, password):
         token = auth.register(username, password)
         if token is not None:
+            pdb = person_setup()
+            person = pdb.query(Person).get(username)
+            if person is None:
+                person = Person()
+                person.username = username
+                pdb.add(person)
+                pdb.commit()
             return self.loginCookie(username, token)
         else:
             return None
