968da55953d2d4350b203f618fcebc4367f4c86b
AleSierra26 <alemsierra26@uc.cl>
2025-11-04 12:43:31 -0300

    nombres 4

--- symex/fuzzy.py
+++ symex/fuzzy.py
@@ -974,15 +974,15 @@ def concolic_find_input(constraint, ok_names, verbose=0):
   ## constraint (i.e., ok == z3.sat), make a new input set
   ## containing the values from Z3's model, and return it.
   (ok, model) = fork_and_check(constraint)
-  cv = ConcreteValues()
+  concrete_values = ConcreteValues()
   if ok == z3.sat:
     sat = True
-    res_names = model.keys() if ok_names is None else ok_names
-    for k in res_names:
-      cv.add(k, model[k])
+    nombres = model.keys() if ok_names is None else ok_names
+    for n in nombres:
+      concrete_values.add(n, model[n])
   else:
     sat = False
-  return sat, cv
+  return sat, concrete_values
 
 # Concolic execute func for many different paths and return all
 # computed results for those different paths.
