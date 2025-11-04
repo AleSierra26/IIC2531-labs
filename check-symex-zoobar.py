c73ee3134dff34c88dd2299171b4239bea7194cf
AleSierra26 <alemsierra26@uc.cl>
2025-11-04 19:29:32 -0300

    parte 9?

--- check-symex-zoobar.py
+++ check-symex-zoobar.py
@@ -91,14 +91,14 @@ def test_stuff():
   ## Detect zoobar theft.
   ## When detected, call report_zoobar_theft()
   username_set = set()
-    for p in pdb.query(zoobar.zoodb.Person).all():
-        username_set.add(p.username)
-    for transfer in tdb.query(zoobar.zoodb.Transfer).all():
-        if transfer.sender in username_set:
+  for p in pdb.query(zoobar.zoodb.Person).all():
+      username_set.add(p.username)
+  for transfer in tdb.query(zoobar.zoodb.Transfer).all():
+      if transfer.sender in username_set:
         username_set.remove(transfer.sender)
-    for username in username_set:
-        person = pdb.query(zoobar.zoodb.Person).get(username)
-        if person.zoobars < 10:
+  for username in username_set:
+      person = pdb.query(zoobar.zoodb.Person).get(username)
+      if person.zoobars < 10:
         report_zoobar_theft()
 
 
